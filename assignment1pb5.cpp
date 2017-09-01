62a1e9bd73082fdc0e77ee5a0ef454b1f4bc73bf
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:05:11 +0530

    Create assignment1pb5.cpp

---
+++ assignment1pb5.cpp
@@ -0,0 +1,14 @@
+# abbasassignment1
+//question 5
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ float l,b,a;
+ cout<<"Enter the length and breadth of rectangle : ";
+ cin>>l>>b;
+ a=l*b;
+ cout<<"Area of rectangle : "<<a;
+ return 0;
+}
