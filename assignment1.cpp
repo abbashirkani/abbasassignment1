44139f751c73cc40dde2adacc24d605fcbb8787f
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 18:51:02 +0530

    Create assignment1.cpp

---
+++ assignment1.cpp
@@ -0,0 +1,29 @@
+# abbasassignment1
+// question 1
+#include<iostream>
+using namespace std;
+int main()
+ {
+ int a;
+ float f;
+ char c;
+ double d;
+ bool b;
+ cout<<"enter integer:";
+ cin>>a;
+ cout<<endl<<"Integer:"<<a;
+ cout<<"enter float:";
+ cin>>f;
+ cout<<"float :"<<f;
+ cout<<"enter char:";
+ cin>>c;
+ cout<<"char :"<<c;
+ cout<<"enter double:";
+ cin>>d;
+ cout<<"double:"<<d;
+ cout<<"enter bool:";
+ cin>>b;
+ cout<<"bool :"<<b;
+ return 0;
+ }
+ 
