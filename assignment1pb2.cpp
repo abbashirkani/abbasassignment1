22b1fd9d0b813f24f6cdc4176c8093cc49873788
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:00:41 +0530

    Create assignment1pb2.cpp

---
+++ assignment1pb2.cpp
@@ -0,0 +1,13 @@
+# abbasassignment1
+//question 2
+#include<<iostream>>
+using namespace std;
+int main()
+{
+ int a, b, c;
+ cout<<"Enter 2 numbers to add"<<endl;
+ cin>>a>>b;
+ c=a+b;
+ cout<<"The sum of two numbers is "<<c<<endl;
+ return 0;
+}
