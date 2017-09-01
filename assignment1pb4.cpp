ec9efda18b7d8a23af5534829abbaf398fa420eb
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:04:07 +0530

    Create assignment1pb4.cpp

---
+++ assignment1pb4.cpp
@@ -0,0 +1,13 @@
+# abbasassignment1
+//question 4
+#include<iostream>
+using namespace std;
+int main()
+{
+ float l,b,p;
+ cout<<"Enter the length and breadth of rectangle : ";
+ cin>>l>>b;
+ p=2*(l+b);
+ cout<<"Perimeter of rectangle : "<<p;
+ return 0;
+}
