6b5cce6fd7dc7ce993c14102ec0c509bf0eb150c
abbashirkani <31324728+abbashirkani@users.noreply.github.com>
2017-09-01 19:11:11 +0530

    Create assignment1pb6.cpp

---
+++ assignment1pb6.cpp
@@ -0,0 +1,19 @@
+# abbasassignment1
+// question 6
+
+#include<iostream>
+using namespace std;
+int main()
+{
+ float r,d,c,a;
+ cout<<"Enter the radius of the circle : ";
+ cin>>r;
+ float pi=3.14;
+ d=2*r;
+ c=pi*d;
+ a=(pi*d*d)/4;
+ cout<<"Diameter of the circle : "<<d<<endl;
+ cout<<"Circumference of the circle : "<<c<<endl;
+ cout<<"Area is equal to : "<<a<<endl;
+ return 0;
+}
